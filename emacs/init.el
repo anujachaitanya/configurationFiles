fe98186694e90142ad5ca67801975a9c97b72225
Anuja Chaitanya <anujachaitanya09@gmail.com>
2024-06-17 15:01:20 +0530

    Updated init file with go lsp and java lsp

--- emacs/init.el
+++ emacs/init.el
@@ -1,46 +1,83 @@
 (require 'package)
-(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/"), ("org" . "https://orgmode.org/elpa")) t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
 
 (setq make-backup-files nil)
 
 (load-theme 'darktooth t)
 (set-background-color '"gray13")
+(set-face-attribute 'default nil :height 140)
+
+;;;; window shortcuts
 (global-set-key (kbd "C-x w") 'delete-frame)
 (global-set-key (kbd "s-<return>") 'toggle-frame-fullscreen)
-
-(require 'helm) ;; finder
-(require 'helm-projectile)
-(require 'helm-swoop)
-(require 'auto-complete)
-(require 'hydra)
-(require 'org)
-
-;; helm key bindings
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-display-line-numbers-mode 1)
-(global-set-key (kbd "C-x C-b") 'helm-projectile-switch-to-buffer)
-
 (global-set-key (kbd "s->") #'other-window)
 (global-set-key (kbd "s-'") #'other-window)
 (global-set-key (kbd "s-<") #'prev-window)
 
+
+(use-package helm-swoop :ensure t)
+(use-package auto-complete :ensure t)
+(use-package hydra :ensure t)
+
+
 (when (string= system-type "darwin")       
   (setq dired-use-ls-dired nil))
 
-(setq display-line-numbers 'relative)
+;;;; line number and scroll bars
+(global-display-line-numbers-mode 1)
+;; (setq display-line-numbers 'relative)
 (scroll-bar-mode -1)
 
-;; alias for yes and no
+
+;;;; alias for yes and no
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq echo-keystrokes 0.1)
 
-;; projectile
+;; ;; which-key
+;; (use-package which-key
+;;   :ensure t
+;;   :config
+;;   (which-key-mode))
+
+;;;; helm
+(use-package helm
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  (global-set-key (kbd "C-x b") 'helm-mini))
+
+;;;; finder
+(use-package helm-projectile
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x C-b") 'helm-projectile-switch-to-buffer)
+  (projectile-global-mode)
+  (helm-projectile-toggle 1)
+  (setq projectile-project-search-path '("~/projects/" "~/my-space/"))
+  (setq projectile-indexing-method 'alien)
+  (setq projectile-switch-project-action 'helm-projectile)
+  (setq projectile-enable-caching t))
+
+;;;; Modes
+
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("^\\*.org\\*$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(add-hook 'org-mode-hook 'org-bullets-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+;;;; hydra projectile
 
 (defhydra hydra-projectile (:color blue)
   "
@@ -67,30 +104,10 @@ _i_ reset cache     _K_ kill all        _D_ root            _R_ regexp replace
   ("s" helm-projectile-ag)
   ("S" projectile-save-project-buffers))
 
-(projectile-global-mode)
-(helm-projectile-toggle 1)
-
-(setq projectile-project-search-path '("~/projects/" "~/my-space/"))
-(setq projectile-indexing-method 'alien)
-(setq projectile-switch-project-action 'helm-projectile)
-
-(setq projectile-enable-caching t)
-
 (global-set-key (kbd "C-c p") 'hydra-projectile/body)
 
-;; Modes
 
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
-
-(add-to-list 'auto-mode-alist '("^\\*.org\\*$" . org-mode))
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
-(add-hook 'org-mode-hook 'org-bullets-mode)
-
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-
-;; git
+;;;; git
 
 (global-git-gutter+-mode t)
 (setq git-gutter+-disabled-modes '(asm-mode image-mode))
@@ -124,6 +141,61 @@ _i_ reset cache     _K_ kill all        _D_ root            _R_ regexp replace
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-c m") 'hydra-magit/body)
 
-;; End for git
+;;;; lsp-mode
+(use-package lsp-mode
+  :ensure t
+  :bind (:map lsp-mode-map
+	      ("C-c d" . lsp-describe-thing-at-point)
+	      ("C-c a" . lsp-execute-code-action))
+  :bind-keymap ("C-c l" . lsp-command-map)
+  ;; :config
+  ;; (lsp-enable-which-key-integration t)
+  )
 
-(message "Hey there!")
+;;;; company
+
+(use-package company
+  :ensure t
+  :hook ((emacs-lisp-mode . (lambda ()
+			      (setq-local company-backends '(company-elisp))))
+	 (emacs-lisp-mode . company-mode))
+  :config
+  (company-keymap--unbind-quick-access company-active-map)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+  (define-key company-active-map (kbd "RET") 'nil)
+  (define-key company-active-map (kbd "<return>") nil)
+  (company-tng-configure-default)
+  (setq company-idle-delay 0.1
+	company-minimum-prefix-length 1))
+
+;;; Go
+(use-package go-mode
+  :ensure t
+  :hook ((go-mode . lsp-deferred)
+	 (go-mode . company-mode))
+  :bind (:map go-mode-map
+	      ("<f6>"  . gofmt)
+	      ("C-c 6" . gofmt))
+  :config
+  (require 'lsp-go)
+  ;; https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+  (setq lsp-go-analyses
+	'((fieldalignment . t)
+	  (nilness        . t)
+	  (unusedwrite    . t)
+	  (unusedparams   . t)))
+  ;; GOPATH/bin
+  (add-to-list 'exec-path "~/go/bin")
+  (setq gofmt-command "goimports"))
+
+
+;;;; Java
+
+(use-package lsp-java
+  :ensure t
+  :init
+  (setq lsp-java-inhibit-message nil)
+  (setq lsp-java-java-path "/usr/local/opt/openjdk@17/bin/java")
+  :hook ((java-mode . lsp-deferred)
+	 (java-mode . company-mode))
+  :config (add-hook 'java-mode-hook 'lsp))
