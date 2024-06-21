9bf74eb3ea800523c86c4c6b0888c8aa48ff6335
anujachaitanya <anujachaitanya09@gmail.com>
2024-06-21 17:49:59 +0530

    Added emacs in installer script along go lsp

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ## brew install 
-brew install alacritty tmux tree openjdk@17 colima docker docker-compose tig autojump
-brew install --cask intellij-idea visual-studio-code 
+brew install alacritty tmux tree openjdk@17 colima docker docker-compose tig autojump go
+brew install --cask visual-studio-code emacs 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -11,11 +11,17 @@ ln -s ./configurationFiles/vimrc .vimrc
 ln -s ./configurationFiles/zshrc .zshrc
 ln -s ./configurationFiles/alacritty.yml .alacritty.yml     
 ln -s ./configurationFiles/tmux.conf .tmux.conf     
+cp ./configurationFiles/emacs/init.el ./.emac.d/init.el
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git .zsh/powerlevel10k
+
+## lsp-servers for emacs
+#  go install golang.org/x/tools/cmd/godoc@latest
+#  go install golang.org/x/tools/cmd/goimports@latest
+#  go install golang.org/x/tools/gopls@latest
 
 source .zshrc
 
